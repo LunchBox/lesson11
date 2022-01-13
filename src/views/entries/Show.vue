f06cb11c60b16f7112b85df1828241ee09e3a131
Daniel Cheang <daniel@somewhere>
2022-01-14 02:53:06 +0800

    remove entry info

--- src/views/entries/Show.vue
+++ src/views/entries/Show.vue
@@ -1,9 +1,8 @@
 <template>
 	<div v-if="entry">
-    {{ entry }}
 		<h2>{{ entry.title }}</h2>
 
-    <EntryItemList :entry="entry" />
+		<EntryItemList :entry="entry" />
 	</div>
 </template>
 
@@ -11,16 +10,16 @@
 	import { ref, computed, toRef } from "vue";
 	import { useRoute } from "vue-router";
 
-  import Entry from "@/models/entry.js";
+	import Entry from "@/models/entry.js";
 
-  import EntryItemList from "../entry_items/List.vue";
+	import EntryItemList from "../entry_items/List.vue";
 
 	const route = useRoute();
 
-  Entry.fetch(route.params.id)
-  const entry = computed(() => Entry.find(route.params.id))
+	Entry.fetch(route.params.id);
+	const entry = computed(() => Entry.find(route.params.id));
 
-  console.log(entry)
+	console.log(entry);
 </script>
 
 <style>
--- src/views/entry_items/ListItem.vue
+++ src/views/entry_items/ListItem.vue
@@ -1,51 +1,54 @@
 <template>
-  <div v-if="entryItem">
+	<div v-if="entryItem">
+		<div v-if="false">
+			{{ entryItem }}
+			&middot;
+			<a @click.prevent="del">Del</a>
+		</div>
 
-    <div v-if="false">
-      {{ entryItem }}
-      &middot;
-      <a @click.prevent="del">Del</a>
-    </div>
-
-    <EntryItemForm v-if="editing" :entryItem="entryItem" @after-submit="editing = false" />
-    <div v-else v-html="content" @dblclick="edit"></div>
-  </div>
+		<EntryItemForm
+			v-if="editing"
+			:entryItem="entryItem"
+			@after-submit="editing = false"
+		/>
+		<div v-else v-html="content" @dblclick="edit"></div>
+	</div>
 </template>
 
 <script setup>
 	import { ref, computed } from "vue";
 	import { marked } from "marked";
 
-  import Entry from "@/models/entry.js";
-  import EntryItem from "@/models/entry_item.js";
+	import Entry from "@/models/entry.js";
+	import EntryItem from "@/models/entry_item.js";
 
-  import EntryItemForm from "./Form.vue";
+	import EntryItemForm from "./Form.vue";
 
-  const props = defineProps({
-    entry: Entry,
-    entryItem: EntryItem
-  });
+	const props = defineProps({
+		entry: Entry,
+		entryItem: EntryItem,
+	});
 
-  const content = computed(() => {
+	const content = computed(() => {
 		if (props.entryItem && props.entryItem.content) {
 			return marked(props.entryItem.content);
 		}
-  });
+	});
 
-  function del(){
-    const entryItem = props.entryItem;
-    const idx = props.entry.entryItemIds.findIndex(id => id === entryItem.id);
-    if (idx > -1) {
-      props.entry.entryItemIds.splice(idx, 1);
-      props.entry.update();
-    }
-    entryItem.destroy();
-  }
+	function del() {
+		const entryItem = props.entryItem;
+		const idx = props.entry.entryItemIds.findIndex((id) => id === entryItem.id);
+		if (idx > -1) {
+			props.entry.entryItemIds.splice(idx, 1);
+			props.entry.update();
+		}
+		entryItem.destroy();
+	}
 
-  const editing = ref(false);
-  function edit(){
-    editing.value = true
-  }
+	const editing = ref(false);
+	function edit() {
+		editing.value = true;
+	}
 </script>
 
 <style>

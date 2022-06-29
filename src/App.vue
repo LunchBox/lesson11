c104c98602f8ca5c617ccf5b0d8aac3413be3528
Daniel Cheang <daniel@somewhere>
2022-06-29 09:36:57 +0800

    tiny fix

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,51 @@
 <script setup>
-	import { ref, computed } from "vue";
-  import { useRoute } from "vue-router";
-	import Config from "@/models/config";
-  
-  import { Icon } from '@vicons/utils'
-  import { OverflowMenuHorizontal, Home, ChevronLeft, Close } from '@vicons/carbon'
+import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
+import Config from "@/models/config";
 
-	const loading = ref(true);
-	Config.preload().then(() => {
-		loading.value = false;
-	});
+import { Icon } from "@vicons/utils";
+import {
+  OverflowMenuHorizontal,
+  Home,
+  ChevronLeft,
+  Close,
+} from "@vicons/carbon";
 
-  const longTermEntries = computed(() => (Config.global && Config.global.entries.filter(e => e).reverse()) || []);
-  const shortTermEntries = computed(() => (Config.global && Config.global.shortTermEntries.filter(e => e).reverse()) || []);
+const loading = ref(true);
+Config.preload().then(() => {
+  loading.value = false;
+});
 
+const longTermEntries = computed(
+  () =>
+    (Config.global && Config.global.entries.filter((e) => e).reverse()) || []
+);
+const shortTermEntries = computed(
+  () =>
+    (Config.global &&
+      Config.global.shortTermEntries.filter((e) => e).reverse()) ||
+    []
+);
 
-  const showAside = ref(false);
+const showAside = ref(true);
 
-	async function removeST(entry) {
-		const g = Config.global;
-    g.stIds.remove(entry.id);
-		await g.update();
-	}
+async function removeST(entry) {
+  const g = Config.global;
+  g.stIds.remove(entry.id);
+  await g.update();
+}
 
-	async function removeLT(entry) {
-		const g = Config.global;
-    g.entryIds.remove(entry.id);
-		await g.update();
-	}
+async function removeLT(entry) {
+  const g = Config.global;
+  g.entryIds.remove(entry.id);
+  await g.update();
+}
 </script>
 
 <template>
-  <div style="display: flex;">
+  <div style="display: flex">
     <aside v-if="showAside">
-      <a href="#" @click.prevent="showAside = false" style="float: right;">
+      <a href="#" @click.prevent="showAside = false" style="float: right">
         <Icon><ChevronLeft /></Icon>
       </a>
       <router-link to="/entries">
@@ -43,7 +55,11 @@
       <ul v-if="!loading" class="menus">
         <li v-for="entry in longTermEntries" :key="entry.id">
           <div>
-            <router-link :key="entry.id" :to="`/entries/${entry.id}`" class="btn">
+            <router-link
+              :key="entry.id"
+              :to="`/entries/${entry.id}`"
+              class="btn"
+            >
               {{ entry.title }}
             </router-link>
             <a href="" class="del btn" @click.prevent="removeLT(entry)">
@@ -62,7 +78,11 @@
             </a>
           </li>
           <li v-for="entry in shortTermEntries" :key="entry.id">
-            <router-link :key="entry.id" :to="`/entries/${entry.id}`" class="btn">
+            <router-link
+              :key="entry.id"
+              :to="`/entries/${entry.id}`"
+              class="btn"
+            >
               {{ entry.title }}
             </router-link>
             <a href="" class="del btn" @click.prevent="removeST(entry)">
@@ -80,151 +100,151 @@
 </template>
 
 <style scoped>
-	.tabs {
-		margin: 0;
-		padding: 0;
-		list-style: none;
-		display: flex;
-    align-items: center;
-    height: 2em;
-	}
-	.tabs li {
-		margin: 0 4px;
-    border-radius: 2px;
-    display: flex;
-	}
-	.tabs li a {
-    display: flex;
-    align-items: center;
+.tabs {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  height: 2em;
+}
+.tabs li {
+  margin: 0 4px;
+  border-radius: 2px;
+  display: flex;
+}
+.tabs li a {
+  display: flex;
+  align-items: center;
 
-		text-decoration: none;
-    padding: 0 4px;
+  text-decoration: none;
+  padding: 0 4px;
 
-    max-width: 120px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-	}
-  .tabs li a:hover {
-    background: #eee;
-  }
+  max-width: 120px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.tabs li a:hover {
+  background: #eee;
+}
 </style>
 
 <style>
-  body{
-    overflow-y: scroll; 
-    margin: 0;
-    padding: 0;
-  }
-	#app {
+body {
+  overflow-y: scroll;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  --p-margin: 0.7rem;
+  --p-margin: 0.25rem;
+  --p-margin: 0;
+  --color: #2c3e50;
+  --color: rgb(55, 53, 47);
+  --font-family: Avenir, Helvetica, Arial, sans-serif;
+  --font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji",
+    "Segoe UI Symbol";
+  --font-size: 15px;
+  --line-height: 1.5;
 
-		--p-margin: 0.7rem;
-		--p-margin: 0.25rem;
-		--p-margin: 0;
-		--color: #2c3e50;
-    --color: rgb(55, 53, 47);
-		--font-family: Avenir, Helvetica, Arial, sans-serif;
-    --font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
-    --font-size: 15px;
-    --line-height: 1.5;
+  color: var(--color);
+  font-family: var(--font-family);
+  font-size: var(--font-size);
+  line-height: var(--line-height);
 
-		color: var(--color);
-    font-family: var(--font-family);
-		font-size: var(--font-size);
-		line-height: var(--line-height);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-	}
+aside {
+  padding: 0.5em;
+  flex: 0 0 280px;
 
-  aside {
-    padding: .5em;
-    flex: 0 0 280px;
+  border-right: 1px solid rgb(204, 213, 221);
+  background: #f5f7fa;
+}
 
-    border-right: 1px solid rgb(204, 213, 221);
-    background: #f5f7fa;
-  }
+main {
+  flex: 1;
+}
+main .container {
+  max-width: 640px;
+  max-width: 56rem;
+  margin: 0 auto;
+  padding: 0 2em;
+}
 
-  main {
-    flex: 1;
-  }
-	main .container{
-		max-width: 640px;
-		max-width: 56rem;
-		margin: 0 auto;
-    padding: 0 2em;
-	}
+a {
+  color: var(--color);
+}
 
-	a {
-		color: var(--color);
-	}
+textarea {
+  color: var(--color);
+  font-family: var(--font-family);
+  font-size: var(--font-size);
+  line-height: var(--line-height);
 
-	textarea {
-		color: var(--color);
-    font-family: var(--font-family);
-		font-size: var(--font-size);
-		line-height: var(--line-height);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: transparent;
+}
 
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		-webkit-text-size-adjust: 100%;
-		-webkit-tap-highlight-color: transparent;
-	}
+footer {
+  height: calc(100vh - 6em);
+}
 
-	footer {
-		height: calc(100vh - 6em);
-	}
+::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: #ccc;
+  opacity: 1; /* Firefox */
+}
 
-	::placeholder {
-		/* Chrome, Firefox, Opera, Safari 10.1+ */
-		color: #ccc;
-		opacity: 1; /* Firefox */
-	}
+.rel-path {
+  font-style: italic;
+  font-size: smaller;
+}
 
-	.rel-path {
-		font-style: italic;
-		font-size: smaller;
-	}
+ul.menus li > div {
+  display: flex;
+}
 
+ul.menus li > div > a {
+  display: block;
+}
 
-  ul.menus li > div {
-    display: flex;
-  }
+ul.menus li > div > a:first-child {
+  flex: 1;
+  margin-right: 2px;
+}
 
-  ul.menus li > div > a {
-    display: block;
-  }
-  
-  ul.menus li > div > a:first-child {
-    flex: 1;
-    margin-right: 2px;
-  }
+ul.menus .btn {
+  padding: 0 4px;
+  border-radius: 2px;
+}
+ul.menus .btn:hover {
+  background: #f5f7fa;
+  background: #e5e8ed;
+}
 
-  ul.menus .btn {
-    padding: 0 4px; 
-    border-radius: 2px;
-  }
-  ul.menus .btn:hover {
-    background: #f5f7fa;
-    background: #e5e8ed;
-  }
+.btn.del {
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  .btn.del {
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.nav-mark {
+  position: absolute;
+  padding: 0.5em;
+}
 
-  .nav-mark {
-    position: absolute;
-    padding: 0.5em;
-  }
-
-  .nav-mark a {
-    color: #ccc;
-  }
-  .nav-mark a:hover {
-    color: var(--color);
-  }
+.nav-mark a {
+  color: #ccc;
+}
+.nav-mark a:hover {
+  color: var(--color);
+}
 </style>
--- src/views/entries/Show.vue
+++ src/views/entries/Show.vue
@@ -1,200 +1,199 @@
 <template>
-	<div v-if="loading">
-		<h2>loading...</h2>
-	</div>
-	<div v-else>
-		<h2>
-      <span @dblclick="addToLongTermEntries" :class="{ active: isMarked }">#LT</span> &middot;
+  <div v-if="loading">
+    <h2>loading...</h2>
+  </div>
+  <div v-else>
+    <h2>
       <span @dblclick="addToShortTermEntries">{{ entry.title }}</span>
     </h2>
 
-		<div class="meta">
-			<span @click.prevent="copyToClipboard">
-				{{ entry.mark }}
-			</span>
+    <div class="meta">
+      <span @click.prevent="copyToClipboard">
+        {{ entry.mark }}
+      </span>
       <a href="#" @click.prevent="fileize">fileize</a>
-		</div>
+      <a href="#" @click.prevent="addToLongTermEntries">bookmark</a>
+    </div>
 
-		<EntryItemList :entry="entry" @after-submit="afterSubmit" />
-	</div>
+    <EntryItemList :entry="entry" @after-submit="afterSubmit" />
+  </div>
 </template>
 
 <script setup>
-	import { ref, computed, onBeforeUnmount } from "vue";
-	import { useRoute, onBeforeRouteUpdate } from "vue-router";
+import { ref, computed, onBeforeUnmount } from "vue";
+import { useRoute, onBeforeRouteUpdate } from "vue-router";
 
-	import copyToClipboard from "@/utils/copy_to_clipboard.js";
+import copyToClipboard from "@/utils/copy_to_clipboard.js";
 
-	import Entry from "@/models/entry.js";
-	import Memo from "@/models/memo.js";
-	import Config from "@/models/config.js";
+import Entry from "@/models/entry.js";
+import Memo from "@/models/memo.js";
+import Config from "@/models/config.js";
 
-	import EntryItemList from "../entry_items/List.vue";
+import EntryItemList from "../entry_items/List.vue";
 
-	import { loadEntry, syncScript } from "./helper";
+import { loadEntry, syncScript } from "./helper";
 
-  import { Icon } from '@vicons/utils'
-  import { Bookmark, BookmarkFilled } from '@vicons/carbon'
+import { Icon } from "@vicons/utils";
+import { Bookmark, BookmarkFilled } from "@vicons/carbon";
 
-	const route = useRoute();
-	const entry = computed(() => Entry.find(route.params.id));
+const route = useRoute();
+const entry = computed(() => Entry.find(route.params.id));
 
-	const loading = ref(true);
-	loadEntry(loading, route.params.id);
+const loading = ref(true);
+loadEntry(loading, route.params.id);
 
-	onBeforeRouteUpdate((to, from) => {
-		loadEntry(loading, to.params.id);
-	});
+onBeforeRouteUpdate((to, from) => {
+  loadEntry(loading, to.params.id);
+});
 
-	console.log(import.meta.env);
-	// const runtimeHandler = (event) => {
-	// 	// console.log(event.data);
-	// 	const typeWhitelist = ["log", "image"];
-	// 	if (
-	// 		event.data &&
-	// 		typeof event.data === "object" &&
-	// 		typeWhitelist.includes(event.data.type)
-	// 	) {
-	// 		const { id, type, data } = event.data;
-	// 		const memo = Memo.find(id);
-	// 		memo.$result = { type, data };
-	// 	}
-	// };
+console.log(import.meta.env);
+// const runtimeHandler = (event) => {
+// 	// console.log(event.data);
+// 	const typeWhitelist = ["log", "image"];
+// 	if (
+// 		event.data &&
+// 		typeof event.data === "object" &&
+// 		typeWhitelist.includes(event.data.type)
+// 	) {
+// 		const { id, type, data } = event.data;
+// 		const memo = Memo.find(id);
+// 		memo.$result = { type, data };
+// 	}
+// };
 
-	// window.addEventListener("message", runtimeHandler);
-	// onBeforeUnmount(() => {
-	// 	window.removeEventListener("message", runtimeHandler);
-	// });
+// window.addEventListener("message", runtimeHandler);
+// onBeforeUnmount(() => {
+// 	window.removeEventListener("message", runtimeHandler);
+// });
 
-	function afterSubmit() {
-		// syncScript(entry.value);
-	}
+function afterSubmit() {
+  // syncScript(entry.value);
+}
 
-	async function addToShortTermEntries() {
-    if (Config.global && entry.value) {
-      if (!Config.global.stIds.includes(entry.value.id)) {
-        Config.global.stIds.push(entry.value.id);
-        await Config.global.update();
-      }
-    }
-	}
-
-	async function addToLongTermEntries() {
-    if (Config.global && entry.value) {
-      if (!Config.global.entryIds.includes(entry.value.id)) {
-        Config.global.entryIds.push(entry.value.id);
-        await Config.global.update();
-      }
-    }
-	}
-
-  const isMarked = computed(() =>{
-    if (Config.global && entry.value) {
-      return Config.global.entryIds.includes(entry.value.id);
-    }
-    return false;
-  });
-
-  async function fileize() {
-    console.log(entry.value);
-    if (entry.value) {
-      const res = await entry.value.fileize();
-      window.open(entry.value.fullPath, '_blank').focus();
+async function addToShortTermEntries() {
+  if (Config.global && entry.value) {
+    if (!Config.global.stIds.includes(entry.value.id)) {
+      Config.global.stIds.push(entry.value.id);
+      await Config.global.update();
     }
   }
+}
+
+async function addToLongTermEntries() {
+  if (Config.global && entry.value) {
+    if (!Config.global.entryIds.includes(entry.value.id)) {
+      Config.global.entryIds.push(entry.value.id);
+      await Config.global.update();
+    }
+  }
+}
+
+const isMarked = computed(() => {
+  if (Config.global && entry.value) {
+    return Config.global.entryIds.includes(entry.value.id);
+  }
+  return false;
+});
+
+async function fileize() {
+  console.log(entry.value);
+  if (entry.value) {
+    const res = await entry.value.fileize();
+    window.open(entry.value.fullPath, "_blank").focus();
+  }
+}
 </script>
 
 <style scoped>
-	:deep(textarea) {
-		width: 100%;
-	}
-	.meta {
-    margin-bottom: 1em;
-		font-family: monospace;
-		color: #999;
-	}
-	.meta > * {
-		color: #999;
-		font-size: smaller;
+:deep(textarea) {
+  width: 100%;
+}
+.meta {
+  margin-bottom: 1em;
+  font-family: monospace;
+  color: #999;
+}
+.meta > * {
+  color: #999;
+  font-size: smaller;
 
-		cursor: pointer;
-    margin-right: .5em;
-	}
+  cursor: pointer;
+  margin-right: 0.5em;
+}
 </style>
 
 <style>
-	pre {
-		white-space: pre-wrap;
-		word-wrap: break-word;
-		tab-size: 4;
+pre {
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  tab-size: 4;
 
-		background: #f5f7fa;
-		line-height: 1.35;
-		border-radius: 2px;
+  background: #f5f7fa;
+  line-height: 1.35;
+  border-radius: 2px;
 
-		margin: var(--p-margin) 0;
-		padding: 0.5em;
-	}
+  margin: var(--p-margin) 0;
+  padding: 0.5em;
+}
 
-	code {
-		color: #eb5757;
-		padding: 0.2em 0.4em;
-		font-size: small;
-	}
+code {
+  color: #eb5757;
+  padding: 0.2em 0.4em;
+  font-size: small;
+}
 
-	pre code {
-		color: #555;
-		font-size: 13px;
-		font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
-		background: #f5f7fa;
-		line-height: 1.35;
-		border-radius: 2px;
-		padding: 0;
-	}
+pre code {
+  color: #555;
+  font-size: 13px;
+  font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+  background: #f5f7fa;
+  line-height: 1.35;
+  border-radius: 2px;
+  padding: 0;
+}
 
-	.hljs {
-		background: transparent !important;
-		font-family: monospace;
-		font-size: 13px;
-	}
+.hljs {
+  background: transparent !important;
+  font-family: monospace;
+  font-size: 13px;
+}
 
-	pre code.hljs {
-		padding: 0 !important;
-	}
+pre code.hljs {
+  padding: 0 !important;
+}
 
-	hr {
-		height: 0;
-		border-right: 0;
-		border-top: 0;
-		border-bottom: 1px solid rgba(208, 208, 208, 1);
-		border-left: 0;
-		margin: calc(1.5rem /2) auto;
-		clear: both;
-	}
+hr {
+  height: 0;
+  border-right: 0;
+  border-top: 0;
+  border-bottom: 1px solid rgba(208, 208, 208, 1);
+  border-left: 0;
+  margin: calc(1.5rem / 2) auto;
+  clear: both;
+}
 
-	blockquote {
-		color: #999;
-	}
+blockquote {
+  color: #999;
+}
 
-  h2 .active {
-    color: tomato;
-  }
+h2 .active {
+  color: tomato;
+}
 </style>
-
 
 <style>
-	iframe.debug-frame {
-		width: 100%;
-		height: 2.5em;
-		border: none;
-		overflow: hidden;
+iframe.debug-frame {
+  width: 100%;
+  height: 2.5em;
+  border: none;
+  overflow: hidden;
 
-		display: none;
-	}
+  display: none;
+}
 </style>
 
-<style >
-	.list-item p {
-		margin: var(--p-margin) 0;
-	}
+<style>
+.list-item p {
+  margin: var(--p-margin) 0;
+}
 </style>
